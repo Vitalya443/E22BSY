b0de295f8d9bfb01cd5b39410dee952d53d698d6
Sando1285 <Starosta1285@gmail.com>
2020-12-07 13:12:13 +0200

    Добавьте файлы проекта.

---
+++ Волинец Виталик 2-13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Волинец_Виталик_2_13
+{
+    class Adress
+    {
+        static void Main(string[] args)
+        {
+            string Country;
+            Console.WriteLine("Страна: ");
+            Country = Console.ReadLine();
+
+            string City;
+            Console.WriteLine("Город: ");
+            City = Console.ReadLine();
+
+            string Street;
+            Console.WriteLine("Улица: ");
+            Street = Console.ReadLine();
+
+            string House;
+            Console.WriteLine("Дом");
+            House = Console.ReadLine();
+
+            string Apartment;
+            Console.WriteLine("Квартира: ");
+            Apartment = Console.ReadLine();
+
+            string Index;
+            Console.WriteLine("Почтовый Индекс: ");
+            Index = Console.ReadLine();
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("RESULT: ");
+            Console.WriteLine("Страна: " + Country);
+            Console.WriteLine("Город: " + City);
+            Console.WriteLine("Улица: " + Street);
+            Console.WriteLine("Дом: " + House);
+            Console.WriteLine("Квартира: " + Apartment);
+            Console.WriteLine("Почтовый индекс: " + Index);
+        }
+    }
+}
